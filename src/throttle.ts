e01b1331d08eac74627efca09db13a92411625c8
Shadi <m.a.n.will125@gmail.com>
2023-10-14 01:34:57 +0200

    FINALLY

--- src/throttle.ts
+++ src/throttle.ts
@@ -4,27 +4,27 @@ interface Config {
     onError?: (error: Error) => void;
 }
 
-type Invoker<I extends (...args: Parameters<I>) => ReturnType<I> | void> = (
-    ...args: Parameters<I>
-) => ReturnType<I> | void;
+type Handler<I extends (...args: any) => any> = (...args: Parameters<I>) => ReturnType<I>;
 
-type Throttle<I extends (...args: Parameters<I>) => ReturnType<I> | void> = Invoker<I> & {
-    cancel: () => void;
-    invoke: Invoker<I>;
-};
+type ThrottleHandlerReturn<I extends Handler<I>, C extends Config> = C['isLeading'] extends true
+    ? ReturnType<I>
+    : ReturnType<I> | void;
 
-export function throttle<I extends (...args: Parameters<I>) => ReturnType<I>>(
+export function throttle<I extends Handler<I>, C extends Config>(
     handler: I,
-    config: Config,
-): Throttle<I> {
+    config: C,
+): {
+    cancel: () => void;
+    invoke: (...args: Parameters<I>) => ThrottleHandlerReturn<I, C>;
+} & ((...args: Parameters<I>) => ThrottleHandlerReturn<I, C>) {
     let timeoutId: ReturnType<typeof setTimeout>;
     let isThrotteled = false;
-    let result: ReturnType<I> | void;
+    let result: ThrottleHandlerReturn<I, C>;
     let lastArgs: Parameters<I>;
 
     const { delay = 0, isLeading = false } = config;
 
-    function invoke(...args: Parameters<I>) {
+    function invoke(...args: Parameters<I>): ThrottleHandlerReturn<I, C> {
         try {
             result = handler.apply(this, args);
         } catch (error) {
@@ -44,7 +44,7 @@ export function throttle<I extends (...args: Parameters<I>) => ReturnType<I>>(
     _throttle.cancel = cancel;
     _throttle.invoke = invoke;
 
-    function _throttle(...args: Parameters<I>) {
+    function _throttle(...args: Parameters<I>): ThrottleHandlerReturn<I, C> {
         if (isLeading) {
             if (isThrotteled) {
                 return result;
